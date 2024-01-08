ec060c213bd8b10f1b1d4a3ece58a66a6ade5d2a
Kei-K23 <keiksl2301@gmail.com>
2024-01-08 10:05:01 +0630

    fix stripe api callback

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,80 +5,91 @@ import { stripe } from "@/lib/stripe";
 import { db } from "@/lib/db";
 
 export async function POST(req: NextRequest) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
-
-  let event: Stripe.Event;
-
   try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.WEBHOOK_SECRET_KEY!
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        body,
+        signature,
+        process.env.WEBHOOK_SECRET_KEY!
+      );
+    } catch (e: any) {
+      return new NextResponse(
+        JSON.stringify({ success: false, error: "Webhook error" }),
+        {
+          status: 500,
+        }
+      );
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    if (event.type === "checkout.session.completed") {
+      const subscription = await stripe.subscriptions.retrieve(
+        session.subscription as string
+      );
+
+      if (!session?.metadata?.orgId) {
+        return new NextResponse(
+          JSON.stringify({ success: false, error: "Org id is missing!" }),
+          {
+            status: 400,
+          }
+        );
+      }
+
+      await db.orgSubscription.create({
+        data: {
+          orgId: session?.metadata?.orgId,
+          stripeCustomerId: subscription.customer as string,
+          stripeSubscriptionId: subscription.id,
+          stripePriceId: subscription.items.data[0].price.id,
+          stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000
+          ),
+        },
+      });
+    }
+
+    if (event.type === "invoice.payment_succeeded") {
+      const subscription = await stripe.subscriptions.retrieve(
+        session.subscription as string
+      );
+
+      await db.orgSubscription.update({
+        where: {
+          stripeSubscriptionId: subscription.id as string,
+        },
+        data: {
+          stripePriceId: subscription.items.data[0].price.id,
+          stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000
+          ),
+        },
+      });
+    }
+    return new NextResponse(
+      JSON.stringify({
+        success: true,
+        error: "Successfully checkout payment",
+      }),
+      {
+        status: 200,
+      }
     );
   } catch (e: any) {
     return new NextResponse(
-      JSON.stringify({ success: false, error: "Webhook error" }),
+      JSON.stringify({
+        success: false,
+        error: "Something went wrong with stripe payment",
+      }),
       {
         status: 500,
       }
     );
   }
-
-  const session = event.data.object as Stripe.Checkout.Session;
-
-  if (event.type === "checkout.session.completed") {
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    );
-
-    if (!session?.metadata?.orgId) {
-      return new NextResponse(
-        JSON.stringify({ success: false, error: "Org id is missing!" }),
-        {
-          status: 400,
-        }
-      );
-    }
-
-    await db.orgSubscription.create({
-      data: {
-        orgId: session?.metadata?.orgId,
-        stripeCustomerId: subscription.customer as string,
-        stripeSubscriptionId: subscription.id,
-        stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(
-          subscription.current_period_end * 1000
-        ),
-      },
-    });
-  }
-
-  if (event.type === "invoice.payment_succeeded") {
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    );
-
-    await db.orgSubscription.update({
-      where: {
-        stripeSubscriptionId: subscription.id as string,
-      },
-      data: {
-        stripePriceId: subscription.items.data[0].price.id,
-        stripeCurrentPeriodEnd: new Date(
-          subscription.current_period_end * 1000
-        ),
-      },
-    });
-  }
-
-  return new NextResponse(
-    JSON.stringify({
-      success: false,
-      error: "Something went wrong with stripe payment",
-    }),
-    {
-      status: 500,
-    }
-  );
 }
